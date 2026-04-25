ade5d322d1bef50c9adfe556811cf917353e33a8
Gorilla-Mode <tobias_olsen02@hotmail.com>
2026-04-25 02:15:34 +0200

    fix: support table name change

--- main.go
+++ main.go
@@ -45,22 +45,24 @@ const (
 )
 
 type (
-	colsNames struct {
+	tableDefinition struct {
 		id    string
 		btype string
 		geom  string
+		table string
 	}
 )
 
-func newColsNames(id string, btype string, geom string) *colsNames {
-	return &colsNames{
+func newColsNames(id string, btype string, geom string, table string) *tableDefinition {
+	return &tableDefinition{
 		id:    id,
 		btype: btype,
 		geom:  geom,
+		table: table,
 	}
 }
 
-var Cols = newColsNames("fid", "key", "wkt_geom")
+var Cols = newColsNames("fid", "key", "wkt_geom", "buildings")
 
 func main() {
 	inDir := "in"
@@ -246,27 +248,35 @@ func calculateCentroid(geometry []Coordinate) (lat, lon float64) {
 }
 
 func setColsNames() {
-	fmt.Println("Set identity:")
-	var id string
-	_, err := fmt.Scan(&id)
+	fmt.Println("Set table name: ")
+	var table string
+	_, err := fmt.Scan(&table)
 	if err != nil {
 		return
 	}
 
-	fmt.Println("Set building type:")
+	fmt.Println("Set identity: ")
+	var id string
+	_, err = fmt.Scan(&id)
+	if err != nil {
+		return
+	}
+
+	fmt.Println("Set building type: ")
 	var btype string
 	_, err = fmt.Scan(&btype)
 	if err != nil {
 		return
 	}
 
-	fmt.Println("Set geometry column name:")
+	fmt.Println("Set geometry column name: ")
 	var geom string
 	_, err = fmt.Scan(&geom)
 	if err != nil {
 		return
 	}
 
+	Cols.table = table
 	Cols.id = id
 	Cols.btype = btype
 	Cols.geom = geom
